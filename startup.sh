844a25407ee15b83a24db11773095116f9181d21
sam <barrett370@gmail.com>
2021-10-10 22:00:08 +0100

    change creds method

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-echo "$creds" | base64 -d > keys.json
-gcloud auth activate-service-account --key-file=keys.json
+# echo "$creds" | base64 -d > keys.json
+gcloud auth activate-service-account --key-file=/creds/keys.json
 cron && tail -f /var/log/cron.log
